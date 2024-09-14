dd11d9b7824d562248293176043d092a85d358b1
Garrett-Ravatt <gravatt@calpoly.edu>
2024-09-14 12:33:54 -0700

    sticky wheel movement

--- levels/test/main-dev/measuring_wheel.gd
+++ levels/test/main-dev/measuring_wheel.gd
@@ -4,13 +4,16 @@ extends Node
 # The measuring wheel overrides the existing character controller.
 # The player moves from hex to hex instead of freely
 
-@onready var player : CharacterBody3D = get_parent()
-@onready var head : Node3D = player.head
+@onready var map : HexMap = $"/root/Node/RenderContainer/SubViewportContainer/SubViewport/Main/HexMap"
+
+@onready var player : CharacterBody3D = $"../"
+@onready var head : Node3D = $"../Head"
+
+var where_to_be : Vector3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#player.call_deferred("set_physics_process", false)
-	pass
+	call_deferred("set_physics_process", false)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -19,4 +22,21 @@ func _process(delta):
 func _physics_process(delta):
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction and (player.position == where_to_be):
+		var where := map.closest_hex(player.position+(direction * map.tile_size))
+		where_to_be = Vector3(where.x, player.position.y, where.y)
 	
+	var dist := where_to_be - player.position
+	if (dist.length() < 0.01):
+		player.position = where_to_be
+	else:
+		var move : Vector3 = dist.normalized() * delta
+		player.position += move
+
+func _input(event):
+	if event.is_action_pressed("s_wheel"):
+		var p := is_physics_processing()
+		call_deferred("set_physics_process", !p)
+		player.call_deferred("set_physics_process", p)
+		var where := map.closest_hex(player.position)
+		where_to_be = Vector3(where.x, player.position.y, where.y)
